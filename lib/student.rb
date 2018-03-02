af4c6316cc5d39aca5d4091557c94b56ab976bc0
Chely Ho <adept_inept@yahoo.com>
2018-03-02 22:35:45 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,9 +3,7 @@ class Student
 
   def self.new_from_db(row)
     student = self.new
-    student.id = row[0]
-    student.name = row [1]
-    student.grade = row[2]
+    student.id, student.name, student.grade = row
     student
   end
 
